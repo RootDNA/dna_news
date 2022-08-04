575cb3aeb5956175d3887f6a366892e61cfc7ec6
rootDNA <aubindna@gmail.com>
2022-08-04 01:42:29 +0100

    added postgres database config

--- app/Http/Controllers/newController.php
+++ app/Http/Controllers/newController.php
@@ -19,8 +19,11 @@ class newController extends Controller
 
         return view("index", compact('articles'));
     }
-    public function details(Request $request)
+    public function details(Request $request, $id)
     {
+        $response = Http::get('https://newsapi.org/v2/everything?q={$id}&from=2022-08-03&sortBy=popularity&apiKey=8e030570c6d946c6ab4d5193c201794d');
+
+        $articles = $response->object()->articles;
 
         $articles = array();
         return view("detail", compact('articles'));
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,7 @@
                 <div class="col s10 offset-s1 offset-m1 m4 l4 offset-l0">
                     <div class="row">
                         <div class="col l12">
-                            <a href="{{ url('/articles/1') }}">
+                            <a href="{{ url('/articles/{{articles->title}}') }}">
                                 <div class="card" style="min-height: 400px">
                                     <div class="card-image">
                                         <img src="{{ $article->urlToImage }}">
